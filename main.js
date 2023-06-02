a6cd48d77097b555260634a8630c47c28a45bf08
Taslim112 <132806178+Taslim112@users.noreply.github.com>
2023-06-02 18:42:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function ShowLoginForm(){
+	
+	SetTitle("Login");
+
+	ShowHideForm("LoginFrom","Show");
+	ShowHideForm("RegistrationFrom","Hide");
+	ShowHideForm("ForgotPasswordForm","Hide");
+
+	ActiveInactiveBtn("ShowLoginBtn","Active");
+	ActiveInactiveBtn("ShowRegistrationBtn","Inactive");
+
+	ShowHideFromSwitchBtn("Show");
+};
+
+function ShowRegistrationForm(){
+	SetTitle("Registration");
+
+	ShowHideForm("RegistrationFrom","Show");
+	ShowHideForm("LoginFrom","Hide");
+	ShowHideForm("ForgotPasswordForm","Hide");
+
+	ActiveInactiveBtn("ShowLoginBtn","Inactive");
+	ActiveInactiveBtn("ShowRegistrationBtn","Active");
+
+	ShowHideFromSwitchBtn("Show");
+};
+
+
+
+function SetTitle(Title){
+	var formTitle = document.getElementById('formTitle');
+	formTitle.innerHTML = Title;
+}
+
+function ShowHideForm(FormID,ShowOrHide){
+	var Form = document.getElementById(FormID);
+
+	if(ShowOrHide == "Show"){
+		Form.style.display = 'block';
+	}else{
+		Form.style.display = 'none';
+	}
+}
+
+
+
+function ShowHideFromSwitchBtn(ShowOrHide) {
+	var formSwitchBtn = document.getElementById('formSwitchBtn');
+	if(ShowOrHide == 'Show'){
+		formSwitchBtn.style.display = '';
+	}else{
+		formSwitchBtn.style.display = 'none';
+	}
+}
